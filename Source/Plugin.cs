bad9bc01fac3d92f3ef72db40d9279da2d32540f
jneb802 <73610029+jneb802@users.noreply.github.com>
2026-06-10 09:25:24 -0700

    Bump valheimCreative to 0.2.10

--- Source/Plugin.cs
+++ Source/Plugin.cs
@@ -12,7 +12,7 @@ namespace ValheimCreative
     {
         internal const string ModGuid = "warpalicious.valheimCreative";
         internal const string ModName = "valheimCreative";
-        internal const string ModVersion = "0.2.9";
+        internal const string ModVersion = "0.2.10";
 
         private readonly Harmony _harmony = new(ModGuid);
 
