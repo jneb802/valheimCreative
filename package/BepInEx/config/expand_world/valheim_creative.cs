431c60d80c70ed7272d042a4478b67ef77b94a49
jneb802 <73610029+jneb802@users.noreply.github.com>
2026-06-09 18:48:43 -0700

    Add creative zone spacing migration

--- package/BepInEx/config/expand_world/valheim_creative.cs
+++ package/BepInEx/config/expand_world/valheim_creative.cs
@@ -1,7 +1,4 @@
-// Expand World Code placeholder.
-// Future custom EWP parameter functions for valheimCreative can live here.
 public static class ValheimCreativeEwc
 {
     public static string CreativeSlot() => "creative_slot_01";
 }
-
